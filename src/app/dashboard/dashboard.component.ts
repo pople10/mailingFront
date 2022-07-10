a5e83b3694aa294d7f363a2f36fbee351de0d721
Mohammed Amine AYACHE <med.amine.ayache@gmail.com>
2022-07-10 22:55:37 +0200

    Adding batch system

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -4,6 +4,7 @@ import { MatCheckboxChange } from '@angular/material/checkbox';
 import { EmailService } from 'app/services/email.service';
 import { HandleResponseService } from 'app/services/handle-response.service';
 import Swal from 'sweetalert2';
+declare var $: any;
 
 @Component({
   selector: 'app-dashboard',
@@ -23,6 +24,39 @@ export class DashboardComponent implements OnInit {
   ssl:boolean=null;
   user:string=null;
   password:string=null;
+
+  limit:number=-1;
+  timeout:number=20;
+  limitType:string="emailBcc";
+  timeouts:any[]=[];
+  sentMails:string[]=[];
+
+  alert(msg,badge)
+  {
+    
+    $.notify({
+      icon: "notifications",
+      message: msg
+
+  },{
+      type: badge,
+      timer: 2000,
+      placement: {
+          from: "top",
+          align: "center"
+      },
+      template: '<div data-notify="container" class="col-xl-4 col-lg-4 col-11 col-sm-4 col-md-4 alert alert-{0} alert-with-icon" role="alert">' +
+        '<button mat-button  type="button" aria-hidden="true" class="close mat-button" data-notify="dismiss">  <i class="material-icons">close</i></button>' +
+        '<i class="material-icons" data-notify="icon">notifications</i> ' +
+        '<span data-notify="title">{1}</span> ' +
+        '<span data-notify="message">{2}</span>' +
+        '<div class="progress" data-notify="progressbar">' +
+          '<div class="progress-bar progress-bar-{0}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div>' +
+        '</div>' +
+        '<a href="{3}" target="{4}" data-notify="url"></a>' +
+      '</div>'
+  });
+  }
 
   constructor(private formBuilder:FormBuilder,private emailService:EmailService,private handleService:HandleResponseService) { }
   ngOnInit() {
@@ -86,20 +120,116 @@ export class DashboardComponent implements OnInit {
       data={...data,emailRecipient:data["emailRecipient"].split("\n").filter(e=>e.trim()!="")};
       data={...data,emailCc:data["emailCc"].split("\n").filter(e=>e.trim()!="")};
       data={...data,emailBcc:data["emailBcc"].split("\n").filter(e=>e.trim()!="")};
+      if(!this.limit||this.limit<0)
+        return this.sendSimpleMail(data);
+      if(!this.limitType)
+      {
+        Swal.fire({
+          title: 'Warning',
+            text: "Please select limit type!",
+            icon: 'warning'
+        });
+        return;
+      } 
+      if(!this.timeout)
+      {
+        Swal.fire({
+          title: 'Warning',
+            text: "Please select time out between batch!",
+            icon: 'warning'
+        });
+        return;
+      }  
+      let size:number=data[this.limitType].length;
+      let i:number=0;
+      console.log("Sending batches");
       this.dataSent=true;
-      this.emailService.sendMail(data).subscribe(response=>{
-          this.handleService.handleSuccess(response);
-      },err=>{this.handleService.handleError(err);})
-      .add(()=>{this.dataSent=false;this.submit=false;});
+      this.submit=true;
+      let flag=true;
+      this.timeouts=[];
+      this.sentMails=[];
+      while(i*this.limit<size&&flag)
+      {
+        let datos=this.emailService.splitArray(data,this.limit,i,this.limitType);
+          let tm = setTimeout(async()=>{
+            this.sendCustomMail(datos,datos["ind"],size).then((res:string[])=>{
+              for(let j of res["data"])
+              {
+                this.sentMails.push(j);
+              }
+              this.alert("Batch number "+res["ind"]+" has been sent","success");
+            }).catch(error=>{
+                flag=false; 
+                for(let i of this.timeouts)
+                {
+                  clearTimeout(i);
+                }
+                this.dataSent=false;
+                this.submit=false;
+                if(i==0)
+                  i=1;
+                Swal.fire({
+                  title: 'Warning',
+                    text: "We could not send all emails ("+(i-1)*this.limit+"/"+size+"):\n "+error,
+                    icon: 'warning'
+                });
+                let out:string=this.emailService.copyRest(data,this.sentMails,this.limitType);
+                this.injectData(out);
+                this.alert("We have replaced the data we didnt send in "+this.limitType+" section","info");
+            });
+          },i*this.timeout*1000);
+          this.timeouts.push(tm);
+        i++;
+      }
     }
     else
     {
       Swal.fire({
         title: 'Warning',
-        text: "The form is not fully valid, check fields!",
-        icon: 'warning'
-    });
+          text: "The form is not fully valid, check fields!",
+          icon: 'warning'
+      });
     }
+  }
+
+  injectData(data:string)
+  {
+    let pp={};
+    pp[this.limitType]=data;
+    this.emailForm.patchValue(pp);
+  }
+  
+
+
+
+  sendSimpleMail(data):void
+  {
+    this.dataSent=true;
+    this.emailService.sendMail(data).subscribe(response=>{
+        this.handleService.handleSuccess(response);
+    },err=>{this.handleService.handleError(err);})
+    .add(()=>{this.dataSent=false;this.submit=false;});
+  }
+
+  sendCustomMail(data,i,size)
+  {
+    this.alert("Batch number "+i+" is going to be treated","info");
+    console.log("Sending request for a batch "+i);
+    return new Promise((resolve, reject) => {
+      this.emailService.sendMail(data).subscribe(response=>{
+        if((i+1)*this.limit>=size)
+        {
+          this.dataSent=false;
+          this.submit=false;
+          Swal.fire({
+            title: 'Warning',
+              text: "Operation done with success",
+              icon: 'success'
+          });
+        }
+        resolve({data:data[this.limitType],ind:i});
+      },err=>{reject(err?.error?.error);});
+    });
   }
 
 }
--- src/app/services/email.service.ts
+++ src/app/services/email.service.ts
@@ -3,16 +3,54 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { BACK_END_DOMAIN } from 'environments/env';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { Clipboard } from '@angular/cdk/clipboard';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmailService {
 
-  constructor(private http:HttpClient,private router:Router) { }
+  constructor(private clipboard: Clipboard,private http:HttpClient,private router:Router) { }
 
   sendMail(loginData:any){
     return this.http.post<any>(`${BACK_END_DOMAIN}/mail/send`,loginData, 
     { headers: new HttpHeaders().set('Content-type', 'application/json') });
+  }
+
+  splitArray(data:any,limit:number,ind:number,comp:string):any
+  {
+    console.log("Spliting data for "+comp);
+    let arr:string[]=data[comp];
+    if(limit*ind>arr.length)
+      {
+        let fin =  {...data};
+        fin[comp]=[];
+        return fin;
+      }
+    let out:string[]=[];
+    for(let i=ind*limit;i<(ind+1)*limit;i++)
+    {
+      if(i>arr.length)
+        break;
+      out.push(arr[i]);
+    }
+    let fin =  {...data,ind:ind};
+    fin[comp]=out.filter(e=>e!=null);
+    return fin;
+  }
+
+  copyRest(data:any,notCopy:string[],comp:string):string
+  {
+    console.log("Copying rest");
+    let arr:string[]=data[comp];
+    let out:string="";
+    console.log(arr,notCopy);
+    for(let i=0;i<arr.length;i++)
+    {
+      if(notCopy.includes(arr[i]))
+        continue;
+      out+=arr[i]+"\n";
+    }
+    return out;
   }
 }
